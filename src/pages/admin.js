9707c0377c8c138fda846366bc51a858030ce639
abir14016 <abir14016@gmail.com>
2023-08-21 01:30:19 +0600

    [DONE] dashboard nested route

--- src/pages/admin.js
+++ src/pages/admin.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import DashboardLayout from '../../components/Layouts/DashboardLayout';
+import RootLayout from '../../components/Layouts/RootLayout';
 
 const AdminPage = () => {
     return (
@@ -9,3 +11,14 @@ const AdminPage = () => {
 };
 
 export default AdminPage;
+
+
+AdminPage.getLayout = function getLayout(page) {
+    return (
+        <RootLayout>
+            <DashboardLayout>
+                {page}
+            </DashboardLayout>
+        </RootLayout>
+    )
+}
